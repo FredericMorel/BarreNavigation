9ed93435c6d284b0743c7ff7e971b9e59b2e683d
Morel Frédéric <frederic.morel.ars@outlook.fr>
2022-03-02 08:26:27 +0400

    Initial Commit

---
+++ Shared/BarreNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarreNavigationApp.swift
+//  Shared
+//
+//  Created by morel on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarreNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
